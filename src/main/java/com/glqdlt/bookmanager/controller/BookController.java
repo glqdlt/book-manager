dbb324edbe78d95bab73d65a4b03afa001d5b5d5
glqdlt <dlfdnd0725@gmail.com>
2018-01-26 18:00:11 +0900

    tags 던져주는 로직추가

--- src/main/java/com/glqdlt/bookmanager/controller/BookController.java
+++ src/main/java/com/glqdlt/bookmanager/controller/BookController.java
@@ -27,6 +27,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RequestMapping("/book")
 @CrossOrigin
 @RestController
 public class BookController {
@@ -39,13 +40,13 @@ public class BookController {
     @Autowired
     BookRepository bookRepository;
 
-    @RequestMapping(value = "/book/search/all", method = RequestMethod.GET)
+    @RequestMapping(value = "/search/all", method = RequestMethod.GET)
     public ResponseEntity<List<BookEntity>> bookSearch() {
         List<BookEntity> list = bookRepository.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/search/{page}", method = RequestMethod.GET)
+    @RequestMapping(value = "/search/{page}", method = RequestMethod.GET)
     public ResponseEntity<Map<String, Object>> bookSearchPage(@PathVariable int page) {
         log.info("page:" + page);
         Pageable pageable = new PageRequest(page, PAGE_COUNT, new Sort(Sort.Direction.DESC, "no"));
@@ -62,7 +63,7 @@ public class BookController {
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/detail/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
     public ResponseEntity<BookEntity> bookDetail(@PathVariable int id) {
         BookEntity bookEntity = bookRepository.findOne(id);
 
@@ -70,14 +71,14 @@ public class BookController {
         return new ResponseEntity<>(bookEntity, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/write", method = RequestMethod.PUT)
+    @RequestMapping(value = "/write", method = RequestMethod.PUT)
     public ResponseEntity<Integer> bookWrite(@RequestBody BookEntity bookEntity) {
         log.info(bookEntity.toString());
         bookRepository.save(bookEntity);
         return new ResponseEntity<>(1, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/update/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Integer> bookUpdate(@PathVariable int id, @RequestBody BookEntity bookEntity) {
         log.info((bookEntity.toString()));
         bookRepository.updateBookEntity(id, bookEntity.getSubject(), bookEntity.getAuthor(), bookEntity.getBook_type(), bookEntity.getNote(), bookEntity
@@ -86,13 +87,13 @@ public class BookController {
         return new ResponseEntity<>(1, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/remove/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/remove/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Integer> bookRemove(@PathVariable int id) {
         bookRepository.deleteByNo(id);
         return new ResponseEntity<>(1, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/book/download/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/download/{id}", method = RequestMethod.GET)
     public ResponseEntity<Object> bookDownload(@PathVariable int id) throws IOException {
 
         // FIXME 실제 데이터베이스에 id 조회를 해서 다운로드 경로를 얻어 오는 로직이 필요하다.
@@ -108,18 +109,20 @@ public class BookController {
         return new ResponseEntity<>(byteArrayResource, headers, HttpStatus.OK);
     }
 
-    @RequestMapping("/upload")
-    public class FileUploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity handleFileUpload(
+            @RequestParam("user-file") MultipartFile multipartFile) throws IOException {
+        String name = multipartFile.getOriginalFilename();
+        System.out.println("File name: " + name);
+        //todo save to a file via multipartFile.getInputStream()
+        byte[] bytes = multipartFile.getBytes();
+        System.out.println("File uploaded content:\n" + new String(bytes));
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
-        @RequestMapping(method = RequestMethod.POST)
-        public ResponseEntity handleFileUpload(
-                @RequestParam("user-file") MultipartFile multipartFile) throws IOException {
-            String name = multipartFile.getOriginalFilename();
-            System.out.println("File name: "+name);
-            //todo save to a file via multipartFile.getInputStream()
-            byte[] bytes = multipartFile.getBytes();
-            System.out.println("File uploaded content:\n" + new String(bytes));
-            return new ResponseEntity(HttpStatus.OK);
-        }
+    @RequestMapping(value = "/search/tags/all", method = RequestMethod.GET)
+    public ResponseEntity<Object> addTags(){
+
+        return new ResponseEntity<>(this.bookRepository.findSubjects(),HttpStatus.OK);
     }
 }
